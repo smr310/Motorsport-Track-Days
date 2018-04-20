8dbd68a729a3522eb5eac21bb1b704c64cfc9310
Steve Romm <smr310@gmail.com>
2018-04-19 21:18:14 -0400

    Add congif.js and add unit test

--- server.js
+++ server.js
@@ -1,4 +1,42 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+const { PORT } = require('./config');
+
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it in run
+let server;
+
+// this function connects to our database, then starts the server
+function runServer(port = PORT) {
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+        })
+        .on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if (err) {
+                return reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
+
+module.exports = { runServer, app, closeServer };
---
+++ test/node-capstone-test.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+// this makes the expect syntax available throughout
+// this module
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+const { app, runServer, closeServer } = require('../server');
+
+describe('BlogPosts API resource', function () {
+
+    // we need each of these hook functions to return a promise
+    // otherwise we'd need to call a `done` callback. `runServer`,
+    // `seedBlogPostData` and `tearDownDb` each return a promise,
+    // so we return the value returned by these function calls.
+    before(function () {
+        return runServer();
+    });
+
+    beforeEach(function () {
+        //return seedBlogPostData();
+    });
+
+    afterEach(function () {
+        //return tearDownDb();
+    });
+
+    after(function () {
+        return closeServer();
+    });
+
+    // note the use of nested `describe` blocks.
+    // this allows us to make clearer, more discrete tests that focus
+    // on proving something small
+    describe('GET endpoint', function () {
+
+        it('should return index html', function () {
+            let res;
+            return chai.request(app)
+                .get('/')
+                .then(function (_res) {
+                    res = _res
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.html;
+
+                })
+        });
+    });
+
+
+});
+
